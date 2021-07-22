db4448c322d5b4ae13d86865951c3d4556ba8e24
Clark <81452611+freezenleo@users.noreply.github.com>
2021-07-22 16:49:25 -0500

    update routes

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+
 
 //api routes
 app.get('/api/notes', (req, res) => {
@@ -30,22 +28,26 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
 
-// app.post('/api/notes', (req, res) => {
-//     req.body.id = animals.length.toString();
+app.post('/api/notes', (req, res) => {
 
-//     const newNote = createNewNote(req.body, notes);
-//     res.json(newNote);
-// })
 
-// function createNewNote(body, noteArr) {
-//     const newNote = body;
-//     noteArr.push(newNote);
-//     fs.writeFileSync(
-//         path.join(__dirname, './db/db.json'),
-//         JSON.stringify({ notes: noteArr }, null, 2)
-//     );
-//     return newNote;
-// }
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+})
+
+function createNewNote(body, noteArr) {
+    const newNote = body;
+    noteArr.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: noteArr }, null, 2)
+    );
+    return newNote;
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 //listener
 app.listen(PORT, () => {
